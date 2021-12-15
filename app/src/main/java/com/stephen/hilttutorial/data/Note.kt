6e02c4369c92a9a6917c2ab5111cf6608407dcb1
StephenLeeDev <stephen.lee.dev@gmail.com>
2021-12-15 23:27:49 +0900

    Add | Generate note data model

---
+++ app/src/main/java/com/stephen/hilttutorial/data/Note.kt
@@ -0,0 +1,15 @@
+package com.stephen.hilttutorial.data
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "Notes")
+data class Note(
+    @PrimaryKey(autoGenerate = true)
+    var id: Long = 0,
+    @ColumnInfo(name = "Title")
+    var title: String,
+    @ColumnInfo(name = "Description")
+    var description: String
+)
